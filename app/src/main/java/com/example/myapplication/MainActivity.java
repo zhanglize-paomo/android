7a78bcc0916aa6e5388b27f77ed6a8386ba2f51b
zhanglize <Z18434395962@163.com>
2019-11-20 15:53:02 +0800

    add(hello world):hello world

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
